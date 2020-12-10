59e4a0e4877b0068570466a668a6a81be5c73f13
Cost6527 <clarice.coliveira@yahoo.com.br>
2020-12-09 22:36:49 -0300

    Connect database

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Pokemon = require('./models/pokemon');
+
+mongoose.connect('mongodb://localhost:27017/pokedex', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind("connection error:"));
+db.once("open", () => {
+    console.log("Database Connected.");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
